d8cad62a81e28caaddf0ac302955d28167cd43c1
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-01-26 19:08:44 +0300

    Add Routes and links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { Route, Link, Routes } from 'react-router-dom';
+import Books from './components/Books';
+import Catagories from './components/Catagories';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <nav>
+        <h1>
+          Bookstore CMS
+        </h1>
+        <ul>
+          <li>
+            <Link to="/"> Books</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
+
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Catagories />} />
+      </Routes>
+    </>
   );
 }
 
