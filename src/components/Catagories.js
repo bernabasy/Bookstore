1624e7d05ae755c597a5ad3a4665d7db959950e3
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-02-02 18:26:24 +0300

    Add styles for Catagories.js file

--- src/components/Catagories.js
+++ src/components/Catagories.js
@@ -9,12 +9,16 @@ const Catagories = () => {
     dispatch(checkStatus());
   };
   return (
-    <>
-      <button type="button" onClick={handelCleck}>
-        Check Status
-      </button>
-      <h1>{categorie}</h1>
-    </>
+    <div className=" m-5 p-5 container h-100">
+      <div className="row h-100 justify-content-center align-items-center ">
+        <div>
+          <button className="m-5 w-25 btn btn-primary" type="button" onClick={handelCleck}>
+            Check Status
+          </button>
+          <h1>{categorie}</h1>
+        </div>
+      </div>
+    </div>
   );
 };
 
