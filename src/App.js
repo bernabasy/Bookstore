f988e3583c0f704693803ab77aea0a823957f19a
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-02-02 18:31:18 +0300

    Add styling for header

--- src/App.js
+++ src/App.js
@@ -10,19 +10,29 @@ function App() {
   dispatch(loadBooksAsync());
   return (
     <>
-      <nav>
-        <h1>
+      <nav className="d-flex ms-3 text-decoration-none align-content-center border-bottom border-secondary ml-3">
+        <h1 className="w-25 mt-3">
           Bookstore CMS
         </h1>
-        <ul>
-          <li>
-            <Link to="/"> Books</Link>
+        <ul className="d-flex w-100 justify-content-between mt-3">
+          <li className="d-flex align-content-center gap-3">
+            <h4 className="mt-3">
+              <Link className=" pt-5 text-reset text-decoration-none" to="/"> Books</Link>
+            </h4>
+            <div className="mt-3 vertical-divider" />
+            <h4 className="mt-3">
+              <Link className="text-reset text-decoration-none" to="/categories">Categories</Link>
+            </h4>
           </li>
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-
+          <div className="float-right">
+            <img
+              className="avatar avatar-64 bg-light rounded-circle text-white p-2"
+              src="https://raw.githubusercontent.com/twbs/icons/main/icons/person-fill.svg"
+              alt="#"
+            />
+          </div>
         </ul>
+
       </nav>
       <Routes>
         <Route path="/" element={<Books />} />
